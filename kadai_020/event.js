d3fa7fb778427575f14965c9005f85dcc640e73d
Sirasu-02 <167178469+Sirasu-02@users.noreply.github.com>
2024-07-03 12:10:00 +0900

    kadai_020アップ

---
+++ kadai_020/event.js
@@ -0,0 +1,12 @@
+// btnというidを持つHTML要素を取得し、定数に代入する
+const addBtn =document.getElementById('btn');
+
+// textというidを持つHTML要素を取得し、定数に代入する
+const  parentElement =document.getElementById('text');
+
+// HTML要素がクリックされたときにイベント処理を実行する
+addBtn.addEventListener('click',() => {
+
+  // parentElementのテキストを変換する
+  parentElement.textContent = 'ボタンをクリックしました';
+});
