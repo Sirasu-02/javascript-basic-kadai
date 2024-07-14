250ef6aa0615a25079776396f60de6504280c2ec
Sirasu-02 <167178469+Sirasu-02@users.noreply.github.com>
2024-07-14 10:06:17 +0900

    Delete kadai_mdn_js directory

--- kadai_mdn_js/mdn.js
+++
@@ -1,7 +0,0 @@
-const today = new Date();
-
-const year = today.getFullYear(); //年
-const month = today.getMonth() + 1; //月
-const date = today.getDate(); //日
-
-console.log(year + '年' + month + '月' + date + '日');
